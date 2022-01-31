fa642f04e8b629da21486151af7379e2b0ac9486
bielfont <94993537+bielfont@users.noreply.github.com>
2022-01-31 21:12:17 +0100

    Update test.js
    Test

--- test.js
+++ test.js
@@ -1,18 +1,18 @@
 
   
-const Bascula = require('./main');
+const alumnos = require('./main');
 
 const assert = require('chai').assert;
 const expect = require('chai').expect;
 const should = require('chai').should();
 
 
-
-describe("Test using ASSERT interface from CHAI module: con datos anotarPeso(73,1.66)-(63,1.36)-(83,1.86)", function() {
+describe(" Test ", function() {
   describe("Check 1: ", function() {
-  it("Chequeando PesoMaximo: assert.equal(value,'value'): debe ser valor 83", function() {
+  it("Chequeando ", function() {
     
-  result   = nota.obtenerPesoMaximo();
-  assert.equal(result, "83");
+  result   = alumnos.OrdenaNom({'alumne': 'Pepe', 'data_naiximent': '2001-02-01'},  {'alumne': 'Rafa', 'data_naiximent': '2005-02-01'}, {'alumne': 'Pedro', 'data_naiximent': '2003-02-01'},  {'alumne': 'Manolo', 'data_naiximent': '2009-02-01'},   {'alumne': 'Juan', 'data_naiximent': '1999-02-01'},   {'alumne': 'Maria', 'data_naiximent': '2007-02-01'})
+  assert.equal(result, "aaa");
   });
   });
+});
