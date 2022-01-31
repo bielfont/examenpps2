bda3dfa0fd798e15885b7f918fa8d58d52c5bd44
Biel Font <biel.font@inphower.com>
2022-01-31 20:41:36 +0100

    99

--- alumnes.js
+++ alumnes.js
@@ -1,24 +1,22 @@
-var dict = {
-	"Alba": "1996-03-02",
-	"Vicente": "1995-01-04",
-	"Borja": "1997-09-13",
-	  };
+var dict = [
+	{'alumne': 'Pepe', 'data_naiximent': '2001-02-01'},
+	{'alumne': 'Rafa', 'data_naiximent': '2005-02-01'},
+	{'alumne': 'Pedro', 'data_naiximent': '2003-02-01'},
+	{'alumne': 'Manolo', 'data_naiximent': '2009-02-01'},
+	{'alumne': 'Juan', 'data_naiximent': '1999-02-01'},
+	{'alumne': 'Maria', 'data_naiximent': '2007-02-01'}
+  
+  ]
   
  
+var items3 = dict.sort((a, b) => a.alumne.localeCompare(b.alumne))
 
-  var items = Object.keys(dict).map(function(key) {
-	return [key, dict[key]];
-  });
-  
-items.sort();
-console.log(items);
+console.log(items3);
 
-var items2 = Object.keys(dict).map(function(key) {
-	return [key, dict[key]];
-  });
-  
-items2.sort();
-console.log(items);
+var items2 = dict.sort((a, b) => a.data_naiximent.localeCompare(b.data_naiximent))
+
+console.log(items2);
+
 
 
 
